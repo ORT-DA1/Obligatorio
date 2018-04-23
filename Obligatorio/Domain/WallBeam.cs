8389d3cfa3f515e48adc913e5badeb17365297d0
Pablo Pereira <invict1@outlook.es>
2018-04-23 19:41:31 -0300

    Creation of test classes

---
+++ Obligatorio/Domain/WallBeam.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    class WallBeam
+    {
+        public int X1 { get; set; }
+        public int Y1 { get; set; }
+
+        public override string ToString()
+        {
+            string format = "x1: {0}, y1:{1}";
+            return String.Format(format, this.X1, this.Y1);
+        }
+    }
+}
---
+++ Obligatorio/UnitTestDomain/WallBeamTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestDomain
+{
+    [TestClass]
+    public class WallBeamTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+
+}

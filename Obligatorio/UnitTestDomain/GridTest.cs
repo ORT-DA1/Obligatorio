7aa6e848a22b11a9c61df4ef726ebe435fc9ddec
Joaquin Touris <jtouris_93@hotmail.com>
2018-04-23 00:07:51 -0300

    Base Starting Project

---
+++ Obligatorio/UnitTestDomain/GridTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestDomain
+{
+    [TestClass]
+    public class GridTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}

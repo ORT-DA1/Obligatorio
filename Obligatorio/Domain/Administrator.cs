fd9d260680ac8d8f7c5cae193c1de8fb4766a725
Pablo Pereira <invict1@outlook.es>
2018-05-04 22:09:14 -0300

    Crear administrador con parametros [REFACTOR]

--- Obligatorio/Domain/Administrator.cs
+++ Obligatorio/Domain/Administrator.cs
@@ -1,16 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Domain
 {
     public class Administrator : User
     {
-        public String IdentityCard { get; set; }
-        public int Phone { get; set; }
-        public String Address { get; set; }
         public DateTime LastAccess { get; set; }
 
         public Administrator() 

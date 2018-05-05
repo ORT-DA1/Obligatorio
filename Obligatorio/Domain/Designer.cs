32045a360c1d6a60f2ac2df3e2b24de7d9e4cd40
Pablo Pereira <invict1@outlook.es>
2018-05-04 21:45:08 -0300

    Crear designer con parametros [GREEN TEST]

--- Obligatorio/Domain/Designer.cs
+++ Obligatorio/Domain/Designer.cs
@@ -8,10 +8,24 @@ namespace Domain
 {
     public class Designer : User
     {
+        
+        public String IdentityCard { get; set; }
+        public int Phone { get; set; }
+        public String Address { get; set; }
+        public DateTime LastAccess { get; set; }
 
         public Designer()
         {
 
+        }
+
+        public Designer(string username, string password, string name, string surname, DateTime registrationDate)
+        {
+            this.Username = username;
+            this.Password = password;
+            this.Name = name;
+            this.Surname = surname;
+            this.RegistrationDate = registrationDate;
         }
     }
 }

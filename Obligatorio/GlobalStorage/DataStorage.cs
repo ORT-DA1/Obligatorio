9d7f5096d558ec83bce121eddd332072393a35ec
Joaquin Touris <jtouris_93@hotmail.com>
2018-05-06 03:54:36 -0300

    Data Storage Class Implementation

--- Obligatorio/GlobalStorage/DataStorage.cs
+++ Obligatorio/GlobalStorage/DataStorage.cs
@@ -1,12 +1,39 @@
-﻿using System;
+﻿ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Domain;
 
 namespace GlobalStorage
 {
-    public class Class1
+    public class DataStorage
     {
+        private static DataStorage storageInstance;
+        public List<Client> Clients { get; }
+        public List<Designer> Designers { get; }
+
+
+        private DataStorage()
+        {
+            this.Clients = new List<Client>();
+            this.Designers = new List<Designer>();
+        }
+
+        public static DataStorage GetStorageInstance()
+        {
+            if (storageInstance == null)
+            {
+                storageInstance = new DataStorage();
+            }
+
+            return storageInstance;
+        }
+
+        public void EmptyStorage()
+        {
+            this.Clients.Clear();
+            this.Designers.Clear();
+        }
     }
 }

0d22a8884ed6f4cb0f079213014dfd348d7d4565
Pablo Pereira <invict1@outlook.es>
2018-05-10 01:22:41 -0300

    Crear WallBeam sin parametros [RED TEST]

---
+++ Obligatorio/UnitTest/WallBeamTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class WallBeamTest
+    {
+        [TestMethod]
+        public void TestCreateWall()
+        {
+            Wall wall = new Wall();
+            Assert.IsNotNull(wall);
+        }
+    }
+}

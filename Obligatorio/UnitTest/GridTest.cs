c0376450d8a24597b1ea6d0edf97b2c676852f31
Pablo Pereira <invict1@outlook.es>
2018-05-10 00:05:10 -0300

    Crear Grid sin parametros [RED TEST]

---
+++ Obligatorio/UnitTest/GridTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class GridTest
+    {
+        [TestMethod]
+        public void TestCreateGridWithoutParameters()
+        {
+            Grid grid = new Grid();
+            Assert.IsNotNull(grid);
+        }
+    }
+}

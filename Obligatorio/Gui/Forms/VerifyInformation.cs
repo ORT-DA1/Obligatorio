ebd3498482fadd74d24bce73f95a9327959c79b4
Joaquin Touris <jtouris_93@hotmail.com>
2018-05-15 03:31:03 -0300

    - Se implemento el Form Verify information para validacion de datos de clientes

---
+++ Obligatorio/Gui/Forms/VerifyInformation.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Forms;
+using Domain.Entities;
+
+namespace Gui.Forms
+{
+    public partial class VerifyInformation : Form
+    {
+        private User user;
+        public VerifyInformation(User user)
+        {
+            InitializeComponent();
+            this.user = user;
+        }
+    }
+}

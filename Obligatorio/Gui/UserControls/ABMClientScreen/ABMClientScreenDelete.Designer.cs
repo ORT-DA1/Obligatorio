4b53fec24cda4f6f43ace23ae12b38493201b62d
Joaquin Touris <jtouris_93@hotmail.com>
2018-05-17 07:00:33 -0300

    - Se modificaron pequenos cambios del client user controller delete

--- Obligatorio/Gui/UserControls/ABMClientScreen/ABMClientScreenDelete.Designer.cs
+++ Obligatorio/Gui/UserControls/ABMClientScreen/ABMClientScreenDelete.Designer.cs
@@ -31,13 +31,14 @@
             this.clientList = new System.Windows.Forms.ListBox();
             this.titleTxt = new System.Windows.Forms.Label();
             this.deleteClient_btn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // clientList
             // 
             this.clientList.Cursor = System.Windows.Forms.Cursors.Hand;
             this.clientList.FormattingEnabled = true;
-            this.clientList.Location = new System.Drawing.Point(202, 53);
+            this.clientList.Location = new System.Drawing.Point(245, 50);
             this.clientList.Name = "clientList";
             this.clientList.Size = new System.Drawing.Size(168, 199);
             this.clientList.TabIndex = 0;
@@ -46,7 +47,7 @@
             // 
             this.titleTxt.AutoSize = true;
             this.titleTxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.titleTxt.Location = new System.Drawing.Point(80, 23);
+            this.titleTxt.Location = new System.Drawing.Point(27, 23);
             this.titleTxt.Name = "titleTxt";
             this.titleTxt.Size = new System.Drawing.Size(38, 20);
             this.titleTxt.TabIndex = 1;
@@ -55,7 +56,7 @@
             // deleteClient_btn
             // 
             this.deleteClient_btn.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.deleteClient_btn.Location = new System.Drawing.Point(246, 267);
+            this.deleteClient_btn.Location = new System.Drawing.Point(289, 264);
             this.deleteClient_btn.Name = "deleteClient_btn";
             this.deleteClient_btn.Size = new System.Drawing.Size(94, 28);
             this.deleteClient_btn.TabIndex = 2;
@@ -63,15 +64,26 @@
             this.deleteClient_btn.UseVisualStyleBackColor = true;
             this.deleteClient_btn.Click += new System.EventHandler(this.deleteClient);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(28, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(192, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Seleccione el Cliente que desea borrar:";
+            // 
             // ABMClientScreenDelete
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.deleteClient_btn);
             this.Controls.Add(this.titleTxt);
             this.Controls.Add(this.clientList);
             this.Name = "ABMClientScreenDelete";
-            this.Size = new System.Drawing.Size(419, 317);
+            this.Size = new System.Drawing.Size(451, 317);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -82,5 +94,6 @@
         private System.Windows.Forms.ListBox clientList;
         private System.Windows.Forms.Label titleTxt;
         private System.Windows.Forms.Button deleteClient_btn;
+        private System.Windows.Forms.Label label1;
     }
 }

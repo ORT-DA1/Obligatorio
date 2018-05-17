9d8c2f9914593f1dc0fd134b20f8ba094c8eab69
Joaquin Touris <jtouris_93@hotmail.com>
2018-05-17 08:24:38 -0300

    Implementacion de UserController Para configuracion de precios

---
+++ Obligatorio/Gui/UserControls/Configuration/ConfigurationPrice.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace Gui.UserControls.Configuration
+{
+    partial class ConfigurationPrice
+    {
+        /// <summary> 
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary> 
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.titleTxt = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // titleTxt
+            // 
+            this.titleTxt.AutoSize = true;
+            this.titleTxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleTxt.Location = new System.Drawing.Point(32, 23);
+            this.titleTxt.Name = "titleTxt";
+            this.titleTxt.Size = new System.Drawing.Size(38, 20);
+            this.titleTxt.TabIndex = 0;
+            this.titleTxt.Text = "Title";
+            // 
+            // ConfigurationPrice
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.titleTxt);
+            this.Name = "ConfigurationPrice";
+            this.Size = new System.Drawing.Size(394, 354);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titleTxt;
+    }
+}
---
+++ Obligatorio/Gui/UserControls/Configuration/ConfigurationPrice.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Gui.Interface;
+
+namespace Gui.UserControls.Configuration
+{
+    public partial class ConfigurationPrice : UserControl, IController
+    {
+        public ConfigurationPrice()
+        {
+            InitializeComponent();
+            this.AccessibleName = "Precios";
+            this.titleTxt.Text = "Configuracion de Precios";
+        }
+        public UserControl GetUserController()
+        {
+            return this;
+        }
+    }
+}

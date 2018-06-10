ec1fd56d0c79e43dca925182a73355fecc74074c
Pablo Pereira <invict1@outlook.es>
2018-06-10 16:29:53 -0300

    Se creó el implementaron metodos de Persistance junto a Context

--- Obligatorio/Persistance/DBContext.cs
+++ Obligatorio/Persistance/DBContext.cs
@@ -1,17 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Domain.Entities;
+using System.Data.Entity;
 
 namespace Persistance
 {
-    public class DBContext
+    public class DBContext : DbContext
     {
+        public DbSet<Designer> Designers { get; set; }
 
         public DBContext()
         {
-
         }
     }
 }

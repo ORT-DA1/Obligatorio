60c0085417fdb6e8caaa71e7e7a58e53073217f4
Pablo Pereira <invict1@outlook.es>
2018-06-10 17:21:29 -0300

    Se implementaron atributos y métodos en ContextDB

---
+++ Obligatorio/Persistance/ContextDB.cs
@@ -0,0 +1,29 @@
+﻿using Domain.Entities;
+using System.Data.Entity;
+
+namespace Persistance
+{
+    public class ContextDB : DbContext
+    {
+        public ContextDB() : base() { }
+
+        public DbSet<Designer> Designers { get; set; }
+
+        public DbSet<Administrator> Administrators { get; set; }
+
+        public DbSet<Architect> Architects { get; set; }
+
+        public DbSet<Client> Clients { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public void EmptyTable()
+        {
+            SaveChanges();
+        }
+
+    }
+}

fbd1940940eac303fd5dcd3c9a89a5a6f8248547
Joaquin Touris <jtouris_93@hotmail.com>
2018-06-10 17:57:34 -0300

    Se creo La clase clientRepository y la interfaz IClientRepository

---
+++ Obligatorio/Persistance/Interface/IClientRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Entities;
+
+namespace Persistance.Interface
+{
+    interface IClientRepository
+    {
+        Client GetClient(Client client);
+        void AddClient(Client client);
+        void ModifyClient(Client clientToModify, Client ModifiedClient);
+        void DeleteClient(Client client);
+        List<Client> GetClients();
+    }
+}
---
+++ Obligatorio/Persistance/RepositoryLogic/ClientRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Persistance.Interface;
+using Domain.Entities;
+
+namespace Persistance.RepositoryLogic
+{
+    public class ClientRepository: IClientRepository
+    {
+        private DBContext context;
+        public ClientRepository(DBContext context)
+        {
+            this.context = context;
+        }
+        public AddClient(Client client)
+        {
+            //context.Clients.Add(client);
+        }
+
+        public Client GetClient(Client client)
+        {
+            //TODO
+            //Client result = context.Clients.Find(client.ClientID);
+            //return (result == null ? Client.NULL : result);
+            return new Client();
+        }
+    }
+}

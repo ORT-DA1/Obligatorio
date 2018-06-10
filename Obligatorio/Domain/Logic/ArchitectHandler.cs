578f7582bbda07d79985d0fcbab90e96e4408276
Joaquin Touris <jtouris_93@hotmail.com>
2018-06-10 12:17:26 -0300

    - Se creo la logica para el menu para ABM arquitecto. - Se creo el Architect Handler para manipular al arquitecto

---
+++ Obligatorio/Domain/Logic/ArchitectHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Interface;
+using Domain.Entities;
+using Domain.Data;
+
+namespace Domain.Logic
+{
+    public class ArchitectHandler
+    {
+        private DataStorage storage;
+        public ArchitectHandler()
+        {
+            this.storage = DataStorage.GetStorageInstance();
+        }
+
+    }
+}

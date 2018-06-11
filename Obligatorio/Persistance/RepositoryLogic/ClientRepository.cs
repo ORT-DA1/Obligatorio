8c8965e06cd37954543ec90b92abe70581f9340c
Joaquin Touris <jtouris_93@hotmail.com>
2018-06-10 23:47:25 -0300

    Se implemento el metodo AddClient para el reposotorio de clientes

--- Obligatorio/Persistance/RepositoryLogic/ClientRepository.cs
+++ Obligatorio/Persistance/RepositoryLogic/ClientRepository.cs
@@ -10,22 +10,19 @@ namespace Persistance.RepositoryLogic
 {
     public class ClientRepository: IClientRepository
     {
-        private DBContext context;
-        public ClientRepository(DBContext context)
+        private ContextDB context;
+        public ClientRepository(ContextDB context)
         {
             this.context = context;
         }
-        public AddClient(Client client)
+        public void AddClient(Client client)
         {
-            //context.Clients.Add(client);
+            context.Clients.Add(client);
         }
 
         public Client GetClient(Client client)
         {
-            //TODO
-            //Client result = context.Clients.Find(client.ClientID);
-            //return (result == null ? Client.NULL : result);
-            return new Client();
+            return context.Clients.Find(client.Id);
         }
     }
 }

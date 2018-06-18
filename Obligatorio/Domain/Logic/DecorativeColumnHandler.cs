0e18765a13fc0084a66a5e8513819b884f65c3d0
Pablo Pereira <invict1@outlook.es>
2018-06-17 21:55:57 -0300

    - Se implementó añadir de DecorativeColumn con sus respectivas validaciones relacionadas a la BD

--- Obligatorio/Domain/Logic/DecorativeColumnHandler.cs
+++ Obligatorio/Domain/Logic/DecorativeColumnHandler.cs
@@ -11,10 +11,16 @@ namespace Domain.Logic
     public class DecorativeColumnHandler : IElementHandler<DecorativeColumn>
     {
         private IDecorativeColumnRepository decorativeColumnRepository;
-        public void Add(DecorativeColumn decorativeColumn)
+        public void Add(Grid grid, DecorativeColumn decorativeColumn)
         {
-            this.decorativeColumnRepository.AddDecorativeColumn(decorativeColumn);
+            this.decorativeColumnRepository.AddDecorativeColumn(grid, decorativeColumn);
         }
 
+        public List<DecorativeColumn> GetList(Grid grid)
+        {
+            List<DecorativeColumn> decorativeColumnList = decorativeColumnRepository.GetAllDecorativeColumns(grid);
+            //
+            return decorativeColumnList;
+        }
     }
 }
--- Obligatorio/Domain/Repositories/DecorativeColumnRepository.cs
+++ Obligatorio/Domain/Repositories/DecorativeColumnRepository.cs
@@ -10,12 +10,20 @@ namespace Domain.Repositories
 {
     public class DecorativeColumnRepository : IDecorativeColumnRepository
     {
-        public void AddDecorativeColumn(DecorativeColumn decorativeColumn)
+        public void AddDecorativeColumn(Grid grid, DecorativeColumn decorativeColumn)
         {
             using (DatabaseContext _context = new Domain.DatabaseContext())
             {
                 _context.DecorativeColumns.Add(decorativeColumn);
                 _context.SaveChanges();
+            }
+        }
+
+        public List<DecorativeColumn> GetAllDecorativeColumns(Grid grid)
+        {
+            using (DatabaseContext _context = new Domain.DatabaseContext())
+            {
+                return _context.DecorativeColumns.Where(d => d.GridId == grid.GridId).ToList();
             }
         }
     }

34d595059f8808a51f0881fe35460cbf7b285f8e
Pablo Pereira <invict1@outlook.es>
2018-06-17 21:54:58 -0300

    - Se implementó añadir de Window con sus respectivas validaciones relacionadas a la BD

--- Obligatorio/Domain/Logic/WindowHandler.cs
+++ Obligatorio/Domain/Logic/WindowHandler.cs
@@ -11,11 +11,17 @@ namespace Domain.Logic
     public class WindowHandler : IElementHandler<Window>
     {
         private IWindowRepository windowRepository;
-        public void Add(Window window)
+        public void Add(Grid grid, Window window)
         {
-            this.windowRepository.AddWindow(window);
+            this.windowRepository.AddWindow(grid, window);
         }
 
+        public List<Window> GetList(Grid grid)
+        {
+            List<Window> windowList = windowRepository.GetAllWindows(grid);
+            //
+            return windowList;
+        }
     }
 
 }

0f9d508b388c5ccc3ffc4d0788afcbc6a08262e9
Pablo Pereira <invict1@outlook.es>
2018-06-19 00:02:32 -0300

    - Se seteó la PK de cada entidad relacionada al grid su relacion con la misma

--- Obligatorio/Domain/DatabaseContext.cs
+++ Obligatorio/Domain/DatabaseContext.cs
@@ -31,6 +31,26 @@ namespace Domain
         {
             modelBuilder.Entity<Grid>().Ignore(g => g.GridStrategy);
 
+            //SETEO DE PRIMARYKEYS
+            modelBuilder.Entity<Grid>().HasKey(g => g.GridId);
+
+            modelBuilder.Entity<Wall>().HasKey(w => w.WallId);
+            modelBuilder.Entity<Grid>().HasMany(g => g.Walls).WithRequired(w => w.Grid);
+
+            modelBuilder.Entity<WallBeam>().HasKey(w => w.WallBeamId);
+            modelBuilder.Entity<Grid>().HasMany(g => g.WallBeams).WithRequired(w => w.Grid);
+
+            modelBuilder.Entity<Window>().HasKey(w => w.WindowId);
+            modelBuilder.Entity<Grid>().HasMany(g => g.Windows).WithRequired(w => w.Grid);
+
+            modelBuilder.Entity<Door>().HasKey(d => d.DoorId);
+            modelBuilder.Entity<Grid>().HasMany(g => g.Doors).WithRequired(w => w.Grid);
+
+            modelBuilder.Entity<DecorativeColumn>().HasKey(d => d.DecorativeColumnId);
+            modelBuilder.Entity<Grid>().HasMany(g => g.DecorativeColumns).WithRequired(w => w.Grid);
+
+
+
         }
     }
 }

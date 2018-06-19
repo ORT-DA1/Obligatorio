80640335f9643e234ac07d4d2c32e353bd7a847d
Pablo Pereira <invict1@outlook.es>
2018-06-19 01:47:17 -0300

    Se implementó el metodo EmptyDataBase y se quitó el seteo de las claves primarias

--- Obligatorio/Domain/DatabaseContext.cs
+++ Obligatorio/Domain/DatabaseContext.cs
@@ -29,28 +29,38 @@ namespace Domain
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
+            #region modelBuilder Ignore 
             modelBuilder.Entity<Grid>().Ignore(g => g.GridStrategy);
+            #endregion
 
-            //SETEO DE PRIMARYKEYS
+            #region modelBuilder PK
             modelBuilder.Entity<Grid>().HasKey(g => g.GridId);
-
             modelBuilder.Entity<Wall>().HasKey(w => w.WallId);
-            modelBuilder.Entity<Grid>().HasMany(g => g.Walls).WithRequired(w => w.Grid);
-
             modelBuilder.Entity<WallBeam>().HasKey(w => w.WallBeamId);
-            modelBuilder.Entity<Grid>().HasMany(g => g.WallBeams).WithRequired(w => w.Grid);
-
             modelBuilder.Entity<Window>().HasKey(w => w.WindowId);
-            modelBuilder.Entity<Grid>().HasMany(g => g.Windows).WithRequired(w => w.Grid);
-
             modelBuilder.Entity<Door>().HasKey(d => d.DoorId);
-            modelBuilder.Entity<Grid>().HasMany(g => g.Doors).WithRequired(w => w.Grid);
-
             modelBuilder.Entity<DecorativeColumn>().HasKey(d => d.DecorativeColumnId);
-            modelBuilder.Entity<Grid>().HasMany(g => g.DecorativeColumns).WithRequired(w => w.Grid);
+            #endregion
 
+            #region modelBuilder Relations
 
+            #endregion
+        }
 
+        public void EmptyDataBase()
+        {
+            Walls.RemoveRange(Walls);
+            Windows.RemoveRange(Windows);
+            WallBeams.RemoveRange(WallBeams);
+            Doors.RemoveRange(Doors);
+            DecorativeColumns.RemoveRange(DecorativeColumns);
+
+            Architects.RemoveRange(Architects);
+            Administrators.RemoveRange(Administrators);
+            Clients.RemoveRange(Clients);
+            Designers.RemoveRange(Designers);
+            
+            SaveChanges();
         }
     }
 }

16c50cc02d612318a18ff9367992183146dc127c
Pablo Pereira <invict1@outlook.es>
2018-06-21 16:51:17 -0300

    Se crearon los manejadores y repositorios para los elementos generados

---
+++ Obligatorio/Domain/Logic/GeneratedDoorHandler.cs
@@ -0,0 +1,34 @@
+﻿using Domain.Entities;
+using Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Logic
+{
+    public class GeneratedDoorHandler
+    {
+        GeneratedDoorRepository generatedDoorRepository;
+
+        public GeneratedDoorHandler()
+        {
+            generatedDoorRepository = new GeneratedDoorRepository();
+        }
+        public List<GeneratedDoor> GetList()
+        {
+            return generatedDoorRepository.GetList();
+        }
+
+        public void Add(Architect architect, GeneratedDoor generatedDoor, PriceAndCost priceAndCost)
+        {
+            this.generatedDoorRepository.Add(architect, generatedDoor, priceAndCost);
+        }
+
+        public GeneratedDoor GetGeneratedDoor(GeneratedDoor generatedDoor)
+        {
+            return generatedDoorRepository.GetGeneratedDoor(generatedDoor);
+        }
+    }
+}
---
+++ Obligatorio/Domain/Logic/GeneratedWindowHandler.cs
@@ -0,0 +1,34 @@
+﻿using Domain.Entities;
+using Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Logic
+{
+    public class GeneratedWindowHandler
+    {
+        GeneratedWindowRepository generatedWindowRepository;
+
+        public GeneratedWindowHandler()
+        {
+            generatedWindowRepository = new GeneratedWindowRepository();
+        }
+        public List<GeneratedWindow> GetList()
+        {
+            return generatedWindowRepository.GetList();
+        }
+
+        public void Add(Architect architect, GeneratedWindow generatedWindow, PriceAndCost priceAndCost)
+        {
+            this.generatedWindowRepository.Add(architect, generatedWindow, priceAndCost);
+        }
+
+        public GeneratedWindow GetGeneratedWindow(GeneratedWindow generatedWindow)
+        {
+            return generatedWindowRepository.GetGeneratedWindow(generatedWindow);
+        }
+    }
+}

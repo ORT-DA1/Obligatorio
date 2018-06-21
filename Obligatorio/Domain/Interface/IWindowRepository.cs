24cdd667e44f39fda19bd5a83accb089c81a74c9
Pablo Pereira <invict1@outlook.es>
2018-06-21 06:37:28 -0300

    Se añadieron métodos en las interfaces para poder relacionar el price/cost de las entidades en la BD

--- Obligatorio/Domain/Interface/IWindowRepository.cs
+++ Obligatorio/Domain/Interface/IWindowRepository.cs
@@ -5,10 +5,11 @@ namespace Domain.Interface
 {
     public interface IWindowRepository
     {
-        void Add(Grid grid, Window window);
+        void Add(Grid grid, Window window, PriceAndCost priceAndCost);
         List<Window> GetList(Grid grid);
         int Count(Grid grid);
         bool Exist(Grid grid, Point ubicationPoint);
         void Remove(Grid grid, Window window);
+        Window GetFirst();
     }
 }
